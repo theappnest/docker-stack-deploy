3a3d523b7a9460a9a33f68386a196193d1b2e443
Sam Teale <sam.teale@drest.com>
2024-09-25 21:13:21 +0100

    chore: support tls

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,30 +2,18 @@
 set -e
 
 # Use root home folder
-SSH_DIR="/root/.ssh"
-SSH_KEY="${SSH_DIR}/docker"
-KNOWN_HOSTS="${SSH_DIR}/known_hosts"
 ENV_FILE_PATH="/root/.env"
 
 login() {
   echo "${PASSWORD}" | docker login "${REGISTRY}" -u "${USERNAME}" --password-stdin
 }
 
-configure_ssh() {
-  mkdir -p "${SSH_DIR}"
-  printf '%s' "UserKnownHostsFile=${KNOWN_HOSTS}" > "${SSH_DIR}/config"
-  chmod 600 "${SSH_DIR}/config"
-}
+configure_tls_files() {
 
-configure_ssh_key() {
-  printf '%s' "$REMOTE_PRIVATE_KEY" > "${SSH_KEY}"
-  lastLine=$(tail -n 1 "${SSH_KEY}")
-  if [ "${lastLine}" != "" ]; then
-    printf '\n' >> "${SSH_KEY}";
-  fi
-  chmod 600 "${SSH_KEY}"
-  eval "$(ssh-agent)"
-  ssh-add "${SSH_KEY}"
+  mkdir /tmp/certs
+  printf '%s' "$REMOTE_CA" > "/tmp/certs/ca.pem"
+  printf '%s' "$REMOTE_CERTIFICATE" > "/tmp/certs/cert.pem"
+  printf '%s' "$REMOTE_PRIVATE_KEY" > "/tmp/certs/key.pem"
 }
 
 configure_env_file() {
@@ -41,22 +29,6 @@ configure_env_file() {
     if [ "${DEBUG}" != "0" ]; then
       echo "Environment vars after: $(env|wc -l)"
     fi
-  fi
-}
-
-configure_ssh_host() {
-  ssh-keyscan -p "${REMOTE_PORT}" "${REMOTE_HOST}" > "${KNOWN_HOSTS}"
-  chmod 600 "${KNOWN_HOSTS}"
-}
-
-connect_ssh() {
-  cmd="ssh"
-  if [ "${SSH_VERBOSE}" != "" ]; then
-    cmd="ssh ${SSH_VERBOSE}"
-  fi
-  user=$(${cmd} -p "${REMOTE_PORT}" "${REMOTE_USER}@${REMOTE_HOST}" whoami)
-  if [ "${user}" != "${REMOTE_USER}" ]; then
-    exit 1;
   fi
 }
 
@@ -138,36 +110,9 @@ if [[ -z "${STACK_NAME}" ]]; then
 fi
 
 
-# CONFIGURE SSH CLIENT
-if configure_ssh > $OUT 2>&1; then
-  echo "SSH client: Configured"
-else
-  echo "SSH client: Configuration failed"
-  exit 1
-fi
-
-if configure_ssh_key > $OUT 2>&1; then
-  echo "SSH client: Added private key"
-else
-  echo "SSH client: Private key failed"
-  exit 1
-fi
-
-if configure_ssh_host > $OUT 2>&1; then
-  echo "SSH remote: Keys added to ${KNOWN_HOSTS}"
-else
-  echo "SSH remote: Server ${REMOTE_HOST} on port ${REMOTE_PORT} not available"
-  exit 1
-fi
-
-if connect_ssh > $OUT; then
-  echo "SSH connect: Success"
-else
-  echo "SSH connect: Failed to connect to remote server"
-  exit 1
-fi
-
-export DOCKER_HOST="ssh://${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PORT}"
+export DOCKER_HOST=tcp://${DOCKER_HOST}:2376
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/tmp/certs
 
 if deploy > $OUT; then
   echo "Deploy: Updated services"
